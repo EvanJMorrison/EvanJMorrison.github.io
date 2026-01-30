63a8fa1830cc4696aaa78dd3a23b596213fc3ef4
Itsapriori <44989460+Itsapriori@users.noreply.github.com>
2026-01-30 22:23:21 +0000

    Deploying to gh-pages from @ EvanJMorrison/EvanJMorrison.github.io@540683b1895577662a1c8209b5b0f0303fb1366f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,18 +440,15 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-have-officially-earned-my-phd-in-molecular-biology",
+          title: 'I have officially earned my PhD in Molecular Biology',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
@@ -511,7 +508,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/EJM%20_curriculum_vitae.pdf", "_blank");
+          window.open("/assets/pdf/EJM_curriculum_vitae.pdf", "_blank");
         },
       },{
         id: 'social-email',
@@ -546,7 +543,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("", "_blank");
         },
       },{
       id: 'light-theme',
